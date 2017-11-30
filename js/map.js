e96cd6efe063e1a4430cc57bfff74b443cc2ae00
Sameh Kamal <sameh.kamal@insight-centre.org>
2017-11-30 16:06:37 +0000

    init commit

--- js/map.js
+++ js/map.js
@@ -3,14 +3,14 @@
  */
 
 // This example displays a marker at the center of Australia.
-// When the user clicks the marker, an info window opens.
+// Whindependence clicks the marker, an info window opens.
 
 function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
+    var dublin = {lat: -8.673180, lng: 51.799761};
 
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 4,
-        center: uluru
+        center: dublin
     });
     d3.tsv("female_records.tsv",
         function(error, data){
@@ -20,9 +20,12 @@ function initMap() {
 
                 var contentString = "<div><h3>"+d.name+"</h3> " +
                     "File reference: "+ d.id +
-                    "<br />Job: "+ d.job +
+                    "<br /> Job: "+ d.job +
                     "<br /> Status: "+ d.status +
                     "<br /> Address: "+ d.address +
+                    "<br /> Easter Rising: "+ d.rising +
+                    "<br /> War of independece : "+ d.independence +
+                    "<br /> Civil war : "+ d.civil +
                     "</div>";
 
                 var infowindow = new google.maps.InfoWindow({
